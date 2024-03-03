2f66654fbed819d08195bdeb366711a27c8f890c
Joaquin Pinto <joaquinpinto@MacBook-Pro-4.local>
2024-03-03 13:53:53 -0600

    census funcion updates

--- analysis/census_api.py
+++ analysis/census_api.py
@@ -3,26 +3,8 @@
 import requests
 import pandas as pd
 
-# testing if it runs in Terminal, keep empty when done
-test_folder = "test/"
-
-# retrieve Census API Key
-CensusAPI_fn = "CensusAPI_key.txt"
-
-with open(CensusAPI_fn, "r") as file:
-    api_key = file.readline().strip()
-    
-# save variables and hostname
-host = "https://api.census.gov/data"
-dataset = "acs/acs5"
-
-year = "2022"
-
-geography = 'TRACT:*'
-state = '17'  # Illinois state code
-
 """
-Variables
+Census Variables
 
 Children under 5
 B01001_003E - Males under 5
@@ -59,15 +41,8 @@ B16010_002E: Population 25 Years and over with less than HS
 
 B16010_041E: Population 25 Years and over with at least Bachelor
 """
-variables = "NAME,B01001_003E,B01001_027E,B02001_001E,B02001_002E,B02001_003E,B02001_005E,B03001_003E,B17001_002E,B19013_001E,B25002_002E,B25003_002E,B07003_004E,B15003_001E,B16010_002E,B16010_041E"
 
-# Construct the API URL
-url = f'{host}/{year}/{dataset}?get={variables}&for={geography}&in=state:{state}&key={api_key}'
-
-response = requests.get(url)
-data = response.json()
-
-col_names = ["DETAILS",
+COL_NAMES = ["DETAILS",
              "MALES_UNDER5",
              "FEMALES_UNDER5",
              "TOTPOP",
@@ -87,7 +62,43 @@ col_names = ["DETAILS",
              "COUNTY",
              "TRACT"]
 
-# save to raw .csv
-df = pd.DataFrame(data[1:], columns=col_names)
-file_path = test_folder + "data/Census_data_raw.csv"
-df.to_csv(file_path, index=False)
+VARIABLES = "NAME,B01001_003E,B01001_027E,B02001_001E,B02001_002E,B02001_003E,B02001_005E,B03001_003E,B17001_002E,B19013_001E,B25002_002E,B25003_002E,B07003_004E,B15003_001E,B16010_002E,B16010_041E"
+
+def retreive_key():
+    """
+    retrieve Census API Key
+    Returns:
+        KEY(str)
+    """
+    CensusAPI_fn = "CensusAPI_key.txt"
+
+    with open(CensusAPI_fn, "r") as file:
+        KEY = file.readline().strip()
+    return KEY
+
+def retreive_census_data(variables=VARIABLES, col_names=COL_NAMES, test=""):
+    """
+    Retreive data from Census API
+    
+    Returns:
+        Save raw data to data/Census_data_raw.csv
+    """
+    host = "https://api.census.gov/data"
+    dataset = "acs/acs5"
+    year = "2022"
+
+    geography = 'TRACT:*'
+    state = '17'  # Illinois state code
+    
+    api_key = retreive_key()
+    
+    # Construct the API URL
+    url = f'{host}/{year}/{dataset}?get={variables}&for={geography}&in=state:{state}&key={api_key}'
+
+    response = requests.get(url)
+    data = response.json()
+    
+    # save to raw .csv
+    df = pd.DataFrame(data[1:], columns=col_names)
+    file_path = test + "data/Census_data_raw.csv"
+    df.to_csv(file_path, index=False)
--- analysis/census_clean.py
+++ analysis/census_clean.py
@@ -3,58 +3,76 @@
 import requests
 import pandas as pd
 
-# testing if it runs in Terminal, keep empty when done
-test_folder = "test/"
+def clean_census_data(test=""):
+    """
+    Clean data from Census API
+    
+    Returns:
+        Save data to data/Census_data.csv
+    """
+    # retreive raw data
+    census_raw_file = test + "data/Census_data_raw.csv"
+    raw_census = pd.read_csv(census_raw_file, dtype=str)
+    raw_census[['Tract_name', 'County_name', 'State_name']] = raw_census["DETAILS"].str.split(';', expand=True)
+    raw_census.head()
 
-# retreive raw data
-census_raw_file = test_folder + "data/Census_data_raw.csv"
-raw_census = pd.read_csv(census_raw_file, dtype=str)
-raw_census[['Tract_name', 'County_name', 'State_name']] = raw_census["DETAILS"].str.split(';', expand=True)
-raw_census.head()
+    # remove rows with tracts that do not have any population
+    raw_census = raw_census[raw_census["TOTPOP"].astype(int) > 0]
 
-# remove rows with tracts that do not have any population
-raw_census = raw_census[raw_census["TOTPOP"].astype(int) > 0]
+    # raw data has -666666666 as the median income if there is no figure available, set this to mean of state"
+    mean_median_income = int(raw_census["MEDIAN_INCOME"][raw_census["MEDIAN_INCOME"].astype(int) > 0].astype(int).mean())
+    raw_census.loc[raw_census["MEDIAN_INCOME"].astype(int) <= 0, "MEDIAN_INCOME"] = str(mean_median_income)
 
-# raw data has -666666666 as the median income if there is no figure available, set this to mean of state"
-mean_median_income = int(raw_census["MEDIAN_INCOME"][raw_census["MEDIAN_INCOME"].astype(int) > 0].astype(int).mean())
-raw_census.loc[raw_census["MEDIAN_INCOME"].astype(int) <= 0, "MEDIAN_INCOME"] = str(mean_median_income)
+    # median income bins
+    med_income_quantiles = raw_census['MEDIAN_INCOME'].astype(int).quantile([0, 1/3, 2/3, 1])
+    labels = ['low', 'medium', 'high']
 
-# median income bins
-med_income_quantiles = raw_census['MEDIAN_INCOME'].astype(int).quantile([0, 1/3, 2/3, 1])
-labels = ['low', 'medium', 'high']
+    # drop the tracts that do not have any population
+    raw_census = raw_census.loc[raw_census["TOTPOP"].astype(int) > 0]
 
-# drop the tracts that do not have any population
-raw_census = raw_census.loc[raw_census["TOTPOP"].astype(int) > 0]
+    # empty datafram for new columns
+    CensusData = pd.DataFrame()
 
-# empty datafram for new columns
-CensusData = pd.DataFrame()
+    # fill columns
+    CensusData["state_name"] = raw_census["State_name"]
+    CensusData["state_code"] = raw_census["STATE"].astype(str)
+    CensusData["county_name"] = raw_census["County_name"]
+    CensusData["county_code"] = raw_census["COUNTY"].astype(str)
+    CensusData["tract_name"] = raw_census["Tract_name"]
+    CensusData["tract_code"] = raw_census["TRACT"].astype(str)
+    CensusData["tot_pop"] = raw_census["TOTPOP"].astype(int)
+    CensusData["pop_under5"] = raw_census["MALES_UNDER5"].astype(int) + raw_census["FEMALES_UNDER5"].astype(int)
+    CensusData["homeowner_rate"] = round(raw_census["HOMEOWNER_OCCUPIED_HOUSES"].astype(int)/raw_census["TOTAL_OCCUPIED_HOUSES"].astype(int), 4)
+    CensusData["less_than_hs_rate"] = round(raw_census["LESS_THAN_HS"].astype(int)/raw_census["POP_OVER25"].astype(int), 4)
+    CensusData["higher_education_rate"] = round(raw_census["BACHELOR_OR_GREATER"].astype(int)/raw_census["POP_OVER25"].astype(int), 4)
+    CensusData["below_poverty_rate"] = round(raw_census["BELOW_POVERTY_LINE"].astype(int)/raw_census["TOTPOP"].astype(int), 4)
+    CensusData["mobility_rate"] = round(raw_census["SAME_HOUSE_AS_LAST_YEAR"].astype(int)/raw_census["TOTPOP"].astype(int), 4)
+    CensusData["income_cat"] = pd.cut(raw_census['MEDIAN_INCOME'].astype(int), bins=med_income_quantiles, labels=labels)
 
-# fill columns
-CensusData["state_name"] = raw_census["State_name"]
-CensusData["state_code"] = raw_census["STATE"].astype(str)
-CensusData["county_name"] = raw_census["County_name"]
-CensusData["county_code"] = raw_census["COUNTY"].astype(str)
-CensusData["tract_name"] = raw_census["Tract_name"]
-CensusData["tract_code"] = raw_census["TRACT"].astype(str)
-CensusData["tot_pop"] = raw_census["TOTPOP"].astype(int)
-CensusData["pop_under5"] = raw_census["MALES_UNDER5"].astype(int) + raw_census["FEMALES_UNDER5"].astype(int)
-CensusData["homeowner_rate"] = round(raw_census["HOMEOWNER_OCCUPIED_HOUSES"].astype(int)/raw_census["TOTAL_OCCUPIED_HOUSES"].astype(int), 4)
-CensusData["less_than_hs_rate"] = round(raw_census["LESS_THAN_HS"].astype(int)/raw_census["POP_OVER25"].astype(int), 4)
-CensusData["higher_education_rate"] = round(raw_census["BACHELOR_OR_GREATER"].astype(int)/raw_census["POP_OVER25"].astype(int), 4)
-CensusData["below_poverty_rate"] = round(raw_census["BELOW_POVERTY_LINE"].astype(int)/raw_census["TOTPOP"].astype(int), 4)
-CensusData["mobility_rate"] = round(raw_census["SAME_HOUSE_AS_LAST_YEAR"].astype(int)/raw_census["TOTPOP"].astype(int), 4)
-CensusData["income_cat"] = pd.cut(raw_census['MEDIAN_INCOME'].astype(int), bins=med_income_quantiles, labels=labels)
+    CensusData["majority_white"] = raw_census.apply(lambda row: is_majority(int(row["WHITE"]), int(row["TOTPOP"])), axis=1)
+    CensusData["majority_black"] = raw_census.apply(lambda row: is_majority(int(row["BLACK"]), int(row["TOTPOP"])), axis=1)
+    CensusData["majority_asian"] = raw_census.apply(lambda row: is_majority(int(row["ASIAN"]), int(row["TOTPOP"])), axis=1)
+    CensusData["majority_hispanic"] = raw_census.apply(lambda row: is_majority(int(row["HISPANIC"]), int(row["TOTPOP"])), axis=1)
+    
+    # Remove census tracts with fewer than 10 children (unneccessary for our analysis)
+    CensusData = CensusData[CensusData['pop_under5'] >= 10]
 
-# define majority as > 50% population in tract identifying as that race
+
+    # save to clean .csv
+    file_path = test + "data/Census_data.csv"
+    CensusData.to_csv(file_path, index=False)
+
 def is_majority(population, total_population):
+    """
+    Helper Function: check if it is majority
+        define majority as > 50% population in tract identifying as that race
+    
+    Input: 
+        population (int): population of that race in the tract
+        total_population (int): total population in that tract
+        
+    Returns:
+        ind (int): 1 if it is majority, 0 if not
+    """
     proportion = population / total_population
     return 1 if proportion > 0.5 else 0
-
-CensusData["majority_white"] = raw_census.apply(lambda row: is_majority(int(row["WHITE"]), int(row["TOTPOP"])), axis=1)
-CensusData["majority_black"] = raw_census.apply(lambda row: is_majority(int(row["BLACK"]), int(row["TOTPOP"])), axis=1)
-CensusData["majority_asian"] = raw_census.apply(lambda row: is_majority(int(row["ASIAN"]), int(row["TOTPOP"])), axis=1)
-CensusData["majority_hispanic"] = raw_census.apply(lambda row: is_majority(int(row["HISPANIC"]), int(row["TOTPOP"])), axis=1)
-
-# save to clean .csv
-file_path = test_folder + "data/Census_data.csv"
-CensusData.to_csv(file_path, index=False)
